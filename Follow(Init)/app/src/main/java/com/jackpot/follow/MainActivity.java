49684fc3b68d4e153cd819d8fb1e838918ddc125
minje46 <minjea1701@naver.com>
2018-05-08 17:11:02 +0900

    Initial_version
    Design only basic structure.

---
+++ Follow(Init)/app/src/main/java/com/jackpot/follow/MainActivity.java
@@ -0,0 +1,98 @@
+package com.jackpot.follow;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
+import android.view.ViewGroup;
+import net.daum.mf.map.api.MapView;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.Button;
+
+
+// Daum map API example.
+    /*
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+       // mapView.setDaumMapApiKey("@string/native_key");
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+
+    }
+}
+*/
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    Tab_schedule F_schedule;
+    Tab_calendar F_calendar;
+    Tab_alarm F_alarm;
+    Tab_setting F_setting;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        F_schedule = new Tab_schedule();
+        F_calendar = new Tab_calendar();
+        F_alarm = new Tab_alarm();
+        F_setting = new Tab_setting();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, F_schedule).commit();
+
+        TabLayout tabs = (TabLayout)findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Schedule"));
+        tabs.addTab(tabs.newTab().setText("Calendar"));
+        tabs.addTab(tabs.newTab().setText("Alarm"));
+        tabs.addTab(tabs.newTab().setText("Setting"));
+
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Log.d("MainActivity", "Selected tab : " +position);
+
+                Fragment selected = null;
+                if(position == 0)
+                    selected = F_schedule;
+                else if(position == 1)
+                    selected = F_calendar;
+                else if(position == 2)
+                    selected = F_alarm;
+                else if(position == 3)
+                    selected = F_setting;
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+    }
+}
---
+++ Follow(Init)/app/src/main/java/com/jackpot/follow/Tab_schedule.java
@@ -0,0 +1,24 @@
+package com.jackpot.follow;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by KWAK on 2018-05-07.
+ */
+
+public class Tab_schedule extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        ViewGroup rootView =(ViewGroup)inflater.inflate(R.layout.tab_schedule, container, false);
+
+        return rootView;
+    }
+}
