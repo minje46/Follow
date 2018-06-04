429e5cea2d2dd19f13c08d6dd429e3ea2d677008
minje46 <minjea1701@naver.com>
2018-06-05 05:45:08 +0900

    Changed a lot.
    At first, changed structure of data flow form object to SQLite database.
    We don't need to use object class anymore.
    And i solved the cursor which has a error to point first index before.

---
+++ Follow_Mid(Solving problems)/app/src/main/java/com/jackpot/follow_init/Tab_alarm.java
@@ -0,0 +1,286 @@
+package com.jackpot.follow_init;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jackpot.follow_init.R;
+
+import static android.media.CamcorderProfile.get;
+
+/**
+ * Created by KWAK on 2018-05-14.
+ */
+
+public class Tab_alarm extends Fragment {
+    SQLiteDatabase database;            // Set a SQLite database
+    Database_alarm database_alarm;      // Call SQLite helper which was over ridden.
+
+    String[] alarmList;                 // 데이테베이스에서 이벤트 이름 빼오는 것. 어레이 어댑터에 넘기는 용도.(알람 리스트)
+    ArrayList<String> al = new ArrayList<String>();         // Set an array list which store alarms.
+    ArrayList<String> ide = new ArrayList<>();         // Set an array list which store id of alarms can discrete others.
+    ListView AlarmList;
+    ViewGroup rootView;
+    Obj_schedule fromThread;
+
+    // Use Firebase.
+    //private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    //private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+  //      final ViewGroup rootView =(ViewGroup)inflater.inflate(R.layout.tab_alarm, container, false);
+
+  //      ListView AlarmList = (ListView)rootView.findViewById(R.id.AlarmList);
+        rootView =(ViewGroup)inflater.inflate(R.layout.tab_alarm, container, false);
+
+        AlarmList = (ListView)rootView.findViewById(R.id.AlarmList);
+
+        FloatingActionButton btnAdd = (FloatingActionButton)rootView.findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Activity alarm call.
+                if (v.getId() == R.id.btnAdd)
+                    startActivityForResult(new Intent(getContext(), Alarm_setting.class), 1);
+            }
+        });
+        // alarm db에 저장 안해도 돌아갈거 같음.
+/*
+        database_alarm = new Database_alarm(rootView.getContext());
+        Cursor cursor = database_alarm.mySelect();
+        while(cursor.moveToNext()) {
+            String k = cursor.getString(0);
+            al.add(k);
+            String i = cursor.getString(1);
+            ide.add(i);
+            Log.d("데이터베이스","task : "+cursor.getString(cursor.getColumnIndex("task")) + "\nId : "+cursor.getInt(cursor.getColumnIndex("id")));
+        }
+*/
+        //final ArrayAdapter<String> aa = new ArrayAdapter<String>(rootView.getContext(), android.R.layout.simple_list_item_checked, al);
+        //AlarmList.setAdapter(aa);
+        AlarmList.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+        AlarmList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                String k = (String)parent.getItemAtPosition(position);
+
+
+                // sql 디비
+                /*
+                database = database_alarm.getWritableDatabase();
+                database.delete(Database_alarm.TABLE_TASK, ide.get(position) + "=" + Database_alarm.KEY_ID, null);
+                */
+                Toast.makeText(rootView.getContext(), k + " Removed", Toast.LENGTH_SHORT).show();
+
+                MainActivity ma = (MainActivity)getActivity();
+                deleteAlarm(ma.hm.get(k));
+                Log.e("알람", "삭제..." + ma.hm.get(k));
+
+                return false;
+            }
+        });
+        return rootView;
+    }
+
+    // 자동 알람.
+    public void setAlarm(Database_overall db, int alarmId, Context context) {
+    //public static void setAlarm(Database_overall db, int alarmId) {
+        double sec_time = 0;
+
+        Calendar cal = Calendar.getInstance();
+        int day_of_week = cal.get(Calendar.DAY_OF_WEEK); // 1.. 일요일, 2.. 월요일 , ... , 7.. 토요일
+
+        Cursor c = db.Select();
+        c.moveToLast();
+        sec_time = c.getDouble(c.getColumnIndex("sec_time"));
+        double sec_hour = sec_time / 60, sec_minute = sec_time % 60;
+
+        cal.set(2018,
+                5,
+                4,
+                (c.getInt(c.getColumnIndex("str_hour")) - (int) sec_hour),
+                (c.getInt(c.getColumnIndex("str_min")) - (int) sec_minute), 00);
+        Log.e("자동알람", "year: " + cal.get(Calendar.YEAR) + ", month: " + cal.get(Calendar.MONTH) + ", day: " + cal.get(Calendar.DAY_OF_MONTH) + ", hour: " + cal.get(Calendar.HOUR_OF_DAY) + ", minute: " + cal.get(Calendar.MINUTE));
+
+
+/*
+        while(c.moveToNext()) {
+            if (c.getInt(c.getColumnIndex("_id")) == alarmId) {
+                sec_time = c.getDouble(c.getColumnIndex("sec_time"));
+            }
+        }
+        Log.e("데이터베이스in알람5", String.valueOf(sec_time));
+        */
+
+
+        Toast.makeText(context, "Alarm is set @ " + cal.getTime(), Toast.LENGTH_LONG).show();
+        Intent callReceiver = new Intent(context, Alarm_receiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, alarmId, callReceiver, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pendingIntent);
+
+
+        alarmList = new String[c.getCount()];
+        int count = 0;
+        if (c.getCount() == 1) {
+            alarmList[count] = c.getString(c.getColumnIndex("event_name"));
+        } else {
+            boolean isFirst = true;
+
+            while (c.moveToNext()) {
+                if (isFirst) {
+                    c.moveToFirst();
+                    isFirst = false;
+                }
+                if (c.getInt(c.getColumnIndex("ala_code")) == 1) {
+                    alarmList[count] = c.getString(c.getColumnIndex("event_name"));
+                    Log.e("되니", "alarmList[" + count + "]: " + alarmList[count]);
+                    count++;
+                }
+            }
+        }
+        c.close();
+
+        if (count != 0) {
+            ArrayAdapter<String> aa = new ArrayAdapter<String>(rootView.getContext(), android.R.layout.simple_list_item_checked, alarmList);
+            AlarmList.setAdapter(aa);
+        }
+        Log.e("알람", "추가..." + String.valueOf(alarmId));
+    }
+
+    // 수동 알람.
+    public void setAlarm(Calendar targetcall, int alarmId){
+        Toast.makeText(getActivity(),"Alarm is set @ "+targetcall.getTime(),Toast.LENGTH_LONG).show();
+        Intent callReceiver = new Intent(getContext(), Alarm_receiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getContext(), alarmId, callReceiver, 0);
+        AlarmManager alarmManager = (AlarmManager)getContext().getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP,targetcall.getTimeInMillis(), pendingIntent);
+        Log.e("알람", "추가..." + String.valueOf(alarmId));
+
+        MainActivity ma = (MainActivity)getActivity();
+        Cursor c = ma.DB_helper.Select();
+        c.moveToFirst();
+        alarmList = new String[c.getCount()];
+        Log.e("되니", "getCount(): " + c.getCount());
+
+        int count = 0;
+        if (c.getCount() == 1) {
+            alarmList[count] = c.getString(c.getColumnIndex("event_name"));
+        }
+        else {
+            boolean isFirst = true;
+
+            while (c.moveToNext()) {
+                if (isFirst) {
+                    c.moveToFirst();
+                    isFirst = false;
+                }
+                if (c.getInt(c.getColumnIndex("ala_code")) == 1) {
+                    alarmList[count] = c.getString(c.getColumnIndex("event_name"));
+                    Log.e("되니", "alarmList[" + count + "]: " + alarmList[count]);
+                    count++;
+                }
+            }
+        }
+        c.close();
+
+        if (count != 0) {
+            ArrayAdapter<String> aa = new ArrayAdapter<String>(rootView.getContext(), android.R.layout.simple_list_item_checked, alarmList);
+            AlarmList.setAdapter(aa);
+        }
+        Log.e("알람", "추가..." + String.valueOf(alarmId));
+
+
+
+
+
+
+    }
+
+    public void deleteAlarm(int alarmId) {
+        Intent callReceiver = new Intent(getContext(), Alarm_receiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getContext(), alarmId, callReceiver, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) getContext().getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(pendingIntent);
+        pendingIntent.cancel();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // request code == 1 is signal from alarmSetting activity after set some input data in alarmSetting and then call this activity to set alarm.
+        if(requestCode == 1){
+            if(!data.getStringExtra("eventName").equals("")) {
+     //           setAlarm((Calendar) data.getExtras().getSerializable("Calendar"), (data.getIntExtra("alarmId", 0)));
+
+                // SQLite Database_alarm 에 Alarm data 저장.
+                /*
+                ContentValues alarmValue = new ContentValues();
+                alarmValue.put("task", data.getStringExtra("eventName"));
+                database_alarm.myInsert(alarmValue);
+*/
+
+                //To store alarm data which is event name and id in hashMap
+                //MainActivity ma = (MainActivity)getActivity();
+                //ma.hm.put(data.getStringExtra("eventName"), (Integer)data.getIntExtra("alarmId",0));
+
+
+                Calendar ex = (Calendar)data.getExtras().getSerializable("Calendar");
+                int hour = ex.get(Calendar.HOUR_OF_DAY);
+                int minute = ex.get(Calendar.MINUTE);
+                Log.e("되니", "hour: " + hour + "minute: " + minute);
+
+                ContentValues values = new ContentValues();
+                values.put("event_name",data.getStringExtra("eventName"));
+                values.put("ala_hour", hour);
+                values.put("ala_min", minute);
+                values.put("ala_code", 1);
+                MainActivity ma = (MainActivity)getActivity();
+                ma.DB_helper.Insert(values);
+
+                Cursor c = ma.DB_helper.Select();
+                c.moveToLast();
+                setAlarm((Calendar) data.getExtras().getSerializable("Calendar"), c.getInt(c.getColumnIndex("_id")));
+
+
+            }
+            else {} // event_name이 null일때 처리 해줘야함.
+        }
+    }
+
+}
---
+++ Follow_Mid(Solving problems)/app/src/main/java/com/jackpot/follow_init/Thread_waySearch.java
@@ -0,0 +1,173 @@
+package com.jackpot.follow_init;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.odsay.odsayandroidsdk.API;
+import com.odsay.odsayandroidsdk.ODsayData;
+import com.odsay.odsayandroidsdk.ODsayService;
+import com.odsay.odsayandroidsdk.OnResultCallbackListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by KWAK on 2018-05-30.
+ */
+
+// Thread 지우기. handler 객체 전달을 위해 runnable 을 이용
+public class Thread_waySearch extends Thread{
+
+    private ODsayService odsayService;
+    private JSONObject jsonObject;
+
+    private HashMap<Integer,String> busType = new HashMap<Integer, String>();
+
+    private Context mContext;
+    private int primaryKey;
+
+    private double time = 0;
+    private double t_dis = 0;
+    private double w_dis = 0;
+
+    SQLiteDatabase database;
+    Database_overall DB_helper;
+
+    public Thread_waySearch(Context context) {
+        this.mContext = context;
+    }
+
+    public Thread_waySearch(Context context, int id){
+        this.mContext = context;
+        primaryKey = id;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        odsayService = ODsayService.init(mContext, mContext.getString(R.string.odsay_API_Key));
+        odsayService.setReadTimeout(5000);
+        odsayService.setConnectionTimeout(5000);
+
+        // Using singleton dp, it makes access database directly on Thread as well.
+        DB_helper = Database_overall.getInstance(mContext);
+        Cursor cursor = DB_helper.Select();
+/*
+        while(cursor.moveToNext()){     // DB 내부 Test Log. (지우지 말 것.)
+            Log.e("데이터베이스in쓰레드","ID : "+cursor.getInt(cursor.getColumnIndex("_id"))
+                    +"\nEvent name : "+cursor.getString(cursor.getColumnIndex("event_name"))
+                    +"\nDept name : "+cursor.getString(cursor.getColumnIndex("dept_name"))
+                    +"\nDept Lati : "+cursor.getDouble(cursor.getColumnIndex("dept_lati"))
+                    +"\nDest name : "+cursor.getString(cursor.getColumnIndex("dest_name"))
+                    +"\nDest Long : "+cursor.getDouble(cursor.getColumnIndex("dest_long"))
+                    +"\nSect time : "+cursor.getInt(cursor.getColumnIndex("sec_time"))
+                    +"\nStart hour : "+cursor.getInt(cursor.getColumnIndex("str_hour"))
+                    +"\nEnd minute : "+cursor.getInt(cursor.getColumnIndex("end_min"))
+                    +"\nAlarm hour : "+cursor.getInt(cursor.getColumnIndex("ala_hour"))
+                    +"\nAlarm minute : "+cursor.getInt(cursor.getColumnIndex("ala_min")));
+        }
+*/
+        // 제대로 되는 거 같긴 한데, 알람이 안울린 경우는 길찾기에서 sec_time 이 계산 안되었을 때인가?
+        while(cursor.moveToNext()){
+            Log.e("데이터베이스in길찾기","ID : "+cursor.getInt(cursor.getColumnIndex("_id")));
+            if(cursor.getInt(cursor.getColumnIndex("_id")) == primaryKey)
+                    odsayService.requestSearchPubTransPath(String.valueOf(cursor.getDouble(cursor.getColumnIndex("dept_long"))), String.valueOf(cursor.getDouble(cursor.getColumnIndex("dept_lati"))), String.valueOf(cursor.getDouble(cursor.getColumnIndex("dest_long"))), String.valueOf(cursor.getDouble(cursor.getColumnIndex("dest_lati"))), "0", "0", "0", onResultCallbackListener);
+        }
+
+        // Thread 종료하는 것. (run 실행 완료하면 자동으로 종료되긴 한다.)
+        //this.interrupt();
+    }
+
+    private OnResultCallbackListener onResultCallbackListener = new OnResultCallbackListener() {
+        @Override
+        public void onSuccess(ODsayData oDsayData, API api) {
+            // jsonObject 는 전체 jason data 저장 객체.
+            jsonObject = oDsayData.getJson();       // 전체 대중교통 길찾기 data 를 Json 으로 불러오는 것.
+            Log.e("Service Test JSON All", jsonObject.toString());   // Json data 제대로 불러왔는지 log.
+
+            try {
+                // path array 는 path 관련 json data 를 array 형태로 저장.
+                JSONArray path = (JSONArray)jsonObject.getJSONObject("result").get("path");
+                Log.d("Service Test Json path", path.toString());    // Path parsing 확인 log.
+
+                // subPath array 는 path 의 child 중 subPath data 를 array 로 저장.
+                JSONArray P_subPath = (JSONArray)path.getJSONObject(0).get("subPath");
+                Log.d("Service Test subPath", P_subPath.toString());    // subPath parsing 확인 log.
+
+                // subPath 의 child 중 traffic 을 저장.
+                ArrayList<JSONObject> C_subPath = new ArrayList<JSONObject>();
+                for(int i = 0; i < P_subPath.length(); i++) {
+                    C_subPath.add(i, P_subPath.getJSONObject(i));
+                    time += C_subPath.get(i).getInt("sectionTime");
+
+                    t_dis += C_subPath.get(i).getInt("distance");
+
+                    if(C_subPath.get(i).getInt("trafficType") == 3)
+                        w_dis += C_subPath.get(i).getInt("distance");
+                }
+
+                for(int i = 0; i < P_subPath.length(); i++)
+                    Log.d("Service Test  traffic", C_subPath.get(i).toString());
+
+
+                // 길찾기 결과 값 Main db에 값저장.
+                database = DB_helper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("sec_time", time);
+                values.put("tot_dis",t_dis);
+                values.put("wak_dis",w_dis);
+                values.put("ala_code", 1);
+                database.update("dataTable", values, "_id=?", new String[]{String.valueOf(primaryKey)});
+
+                Cursor cursor = DB_helper.Select();
+                cursor.moveToLast();
+
+                //Tab_alarm.setAlarm(database_overall, primaryKey);
+                // 길찾기 완료 후, Tab_alarm 호출. (For setting alarm automatically)
+                Tab_alarm ta = new Tab_alarm();
+                ta.setAlarm(DB_helper, primaryKey, mContext);
+
+                // Test 다시 해보기. 몇개 결과값 JSON에서 제대로 안뽑아옴.
+                Log.d("Service Test tot_time ",String.valueOf(time));
+                Log.d("Service Test tot_dis ",String.valueOf(t_dis));
+                Log.d("Service Test tot_wdis ",String.valueOf(w_dis));
+
+                Log.d("Service Test - Type", String.valueOf(C_subPath.get(0).getInt("trafficType")));
+                Log.d("Service Test - Time", String.valueOf(C_subPath.get(0).getInt("sectionTime")));
+                Log.d("Service Test - BusNo", C_subPath.get(1).getJSONArray("lane").getJSONObject(0).getString("busNo").toString());
+                Log.d("Service Test - BusType", String.valueOf(C_subPath.get(1).getJSONArray("lane").getJSONObject(0).getInt("type")));
+
+                Log.d("Service Test traffic2", C_subPath.get(2).toString());
+
+                if(busType.containsKey(C_subPath.get(1).getJSONArray("lane").getJSONObject(0).getInt("type")))
+                    Log.d("Service Test  compare",busType.get(C_subPath.get(1).getJSONArray("lane").getJSONObject(0).getInt("type")));
+                else
+                    Log.d("Service Test compare", "Not exist");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onError(int i, String errorMessage, API api) {
+            Log.d("Service Test", "Result call back fail");
+        }
+    };
+}
