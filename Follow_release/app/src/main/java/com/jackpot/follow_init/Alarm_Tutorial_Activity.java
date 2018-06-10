92e56053745d2a3b734af37fd3e3fba5f999b5f5
minje46 <minjea1701@naver.com>
2018-06-11 08:50:27 +0900

    Release success.
    .

---
+++ Follow_release/app/src/main/java/com/jackpot/follow_init/Alarm_Tutorial_Activity.java
@@ -0,0 +1,29 @@
+package com.jackpot.follow_init;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+/**
+ * Created by ockkj on 2018-06-10.
+ */
+
+public class Alarm_Tutorial_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarm_tutorial_activity);
+
+        Button btn = findViewById(R.id.close);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
---
+++ Follow_release/app/src/main/java/com/jackpot/follow_init/Database_search.java
@@ -0,0 +1,63 @@
+package com.jackpot.follow_init;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by KWAK on 2018-06-09.
+ */
+
+public class Database_search extends SQLiteOpenHelper{
+    public static final int DATABASE_VERSION=1;
+    public static final String DATABASE_NAME="Result";
+
+    public Database_search(Context context, SQLiteDatabase.CursorFactory factory){
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "Create table dataResult ("+
+                "_id integer primary key autoincrement,"+
+                "fk integer,"+
+                "path_type integer,"+
+                "traffic_type integer,"+
+                "section_time integer,"+
+                "section_dis integer,"+
+                "start_name text,"+
+                "end_name text,"+
+                "way_code integer,"+
+                "bus_no Text,"+
+                "type integer);";
+
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "Drop table if exists dataTable";
+        db.execSQL(sql);
+        onCreate(db);
+    }
+
+    public void Insert(ContentValues cv){
+        getWritableDatabase().insert("dataResult", null, cv);
+    }
+
+    public void Delete(String id) {
+        getWritableDatabase().delete("dataResult", "_id=?", new String[]{id});
+    }
+
+    public void Update(String id, ContentValues values) {
+        getWritableDatabase().update("dataResult", values, "_id=?", new String[]{id});
+    }
+
+    public Cursor Select() {
+        Cursor cursor = getReadableDatabase().rawQuery("Select * from dataResult", null);
+        return cursor;
+    }
+}
